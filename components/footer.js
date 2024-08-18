3801e0dbf9b87bfff91f50498df3ae6becbea5e7
Shivram kashyap <kashyapshivram512@gmail.com>
2024-08-18 10:03:55 +0530

    comit

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div style={{ position: "absolute", background: 'black', left: '0', width: '100%', textAlign: 'center' }}>
+        <div style={{ background: 'black', left: '0', bottom: '0', width: '100%', textAlign: 'center' }}>
             <footer className="text-white text-center py-3" y>
                 <p>&copy; 2024 Rento. All rights reserved.</p>
             </footer>
